a4da30c7f2f17d1410a5d95b28ef534d07d3121f
Tabrez Basha <tabrez.basha@fulfil.io>
2018-07-04 23:21:04 +0530

    functionality done

--- src/App.js
+++ src/App.js
@@ -1,74 +1,130 @@
 import React, { Component } from 'react';
-
-import Counter from "./Counter";
-import './App.css';
 import Icon from '@material-ui/core/Icon';
 import Grid from '@material-ui/core/Grid';
-
+import style from './App.css';
 class App extends Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      room_counter: 1,
+      adult_counter: 1,
+      child_counter: 0
     };
   }
-   incrementCounter = () => {
-    this.setState({
-      counter: ++this.state.counter
-    });
+  incrementCounter = (type) => {
+    switch (type) {
+      case 'room':
+        this.setState({
+          room_counter: this.state.room_counter + 1
+        }, () => this.setState({ adult_counter: this.state.adult_counter + 4 }));
+        break;
+      case 'adult':
+        this.setState({
+          adult_counter: this.state.adult_counter + 1
+        }, () => {
+          if (this.state.adult_counter % 4 === 1) {
+            this.setState({ room_counter: this.state.room_counter + 1 })
+          }
+        });
+        break;
+      case 'child':
+        this.setState({
+          child_counter: this.state.child_counter + 1
+        });
+        break;
+      default: break;
+    }
   };
 
-  decrementCounter = () => {
-    this.setState({
-      counter: --this.state.counter
-    });
+  decrementCounter = (type) => {
+    switch (type) {
+      case 'room':
+        if (this.state.room_counter > 1) {
+          this.setState({
+            room_counter: this.state.room_counter - 1
+          }, () => {
+            this.setState({ adult_counter: (this.state.room_counter * 4) })
+          });
+        } else {
+          alert('Cannot be zero');
+        }
+        break;
+      case 'adult':
+        if (this.state.adult_counter > 1) {
+          this.setState({
+            adult_counter: this.state.adult_counter - 1
+          }, () => {
+            if (this.state.adult_counter % 4 === 0) {
+              this.setState({ room_counter: this.state.room_counter - 1 })
+            }
+          });
+        } else {
+          alert('Cannot be zero');
+        }
+        break;
+      case 'child':
+        if (this.state.child_counter > 0) {
+          this.setState({
+            child_counter: this.state.child_counter - 1
+          });
+        } else {
+          alert('Cannot be in negative');
+        }
+        break;
+      default: break;
+    }
   };
+
   render() {
     return (
       <div className="App">
         <p color="primary">Choose number of <b>people</b></p>
-          <div className="board">
-            <Grid container spacing={16}>
-              <Grid item xs={9}>
-                <p color="primary">Rooms</p>
-              </Grid>
-              <Grid item xs={1}>
-                <p className="count" onClick={this.decrementCounter} >
-                  <Icon  color="primary">
+        <div className="board">
+          <Grid container spacing={16}>
+            <Grid item xs={9}>
+              <p color="primary">Rooms</p>
+            </Grid>
+            <Grid item xs={1}>
+              <p className="count" onClick={() => this.decrementCounter('room')} >
+                <Icon color="primary">
                   remove_circle
                   </Icon>
-                </p>
-              </Grid>
-              <Grid item xs={1}>
-                <Counter counter={this.state.counter} />  
-              </Grid>
-              <Grid item xs={1}>
-                <p className="count" onClick={this.incrementCounter} >
-                  <Icon  color="secondary">
+              </p>
+            </Grid>
+            <Grid item xs={1}>
+              <span className="count">
+                {this.state.room_counter}
+              </span>
+            </Grid>
+            <Grid item xs={1}>
+              <p className="count" onClick={() => this.incrementCounter('room')} >
+                <Icon color="secondary">
                   add_circle
                   </Icon>
-                </p>
-              </Grid>
+              </p>
             </Grid>
+          </Grid>
 
-                      <Grid className="adult" container spacing={16}>
+          <Grid className="adult" container spacing={16}>
             <Grid item xs={9}>
               <p color="primary">Adults</p>
             </Grid>
             <Grid className="alinn" item xs={1}>
-              <p className="count" onClick={this.decrementCounter} >
-                <Icon  color="primary">
-                remove_circle
+              <p className="count" onClick={() => this.decrementCounter('adult')} >
+                <Icon color="primary">
+                  remove_circle
                 </Icon>
               </p>
             </Grid>
             <Grid className="alinn" item xs={1}>
-              <Counter counter={this.state.counter} />  
+              <span className="count">
+                {this.state.adult_counter}
+              </span>
             </Grid>
             <Grid className="alinn" item xs={1}>
-              <p className="count" onClick={this.incrementCounter} >
-                <Icon  color="secondary">
-                add_circle
+              <p className="count" onClick={() => this.incrementCounter('adult')} >
+                <Icon color="secondary">
+                  add_circle
                 </Icon>
               </p>
             </Grid>
@@ -79,24 +135,26 @@ class App extends Component {
               <p color="primary">Children</p>
             </Grid>
             <Grid className="alinn" item xs={1}>
-              <p className="count" onClick={this.decrementCounter} >
-                <Icon  color="primary">
-                remove_circle
+              <p className="count" onClick={() => this.decrementCounter('child')} >
+                <Icon color="primary">
+                  remove_circle
                 </Icon>
               </p>
             </Grid>
             <Grid className="alinn" item xs={1}>
-              <Counter counter={this.state.counter} />  
+              <span className="count">
+                {this.state.child_counter}
+              </span>
             </Grid>
             <Grid className="alinn" item xs={1}>
-              <p className="count" onClick={this.incrementCounter} >
-                <Icon  color="secondary">
-                add_circle
+              <p className="count" onClick={() => this.incrementCounter('child')} >
+                <Icon color="secondary">
+                  add_circle
                 </Icon>
               </p>
             </Grid>
           </Grid>
-          </div>
+        </div>
       </div>
     );
   }
